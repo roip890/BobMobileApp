fbc155b02d73c3f0d239f17e29fa7684ec354e81
roip890 <roip890@gmail.com>
2017-12-03 21:17:12 +0200

    3.12

--- app/src/main/java/com/bob/bobmobileapp/tools/UI/MyDateTextView.java
+++ app/src/main/java/com/bob/bobmobileapp/tools/UI/MyDateTextView.java
@@ -1,10 +1,10 @@
 package com.bob.bobmobileapp.tools.UI;
 
-import android.app.TimePickerDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.TimePicker;
+import android.widget.DatePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,11 +16,12 @@ import java.util.Locale;
 
 public class MyDateTextView extends MyTextView {
 
-    protected int hours, minutes, seconds;
+    private int years;
+    private int months;
+    private int days;
     protected SimpleDateFormat dateFormat;
     protected Calendar calendar;
-    protected TimePickerDialog timeDialog;
-    protected Boolean fullHours;
+    protected DatePickerDialog dateDialog;
 
     public MyDateTextView(Context context) {
         this(context, null);
@@ -30,31 +31,32 @@ public class MyDateTextView extends MyTextView {
         this(context, attrs, 0);
     }
 
-    public MyDateTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MyDateTextView(Context context, AttributeSet attrs, final int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.hours = 0;
-        this.minutes = 0;
-        this.seconds = 0;
-        this.fullHours = true;
+        this.setYears(0);
+        this.setMonths(0);
+        this.setDays(0);
         this.dateFormat = new SimpleDateFormat("hh:mm", Locale.getDefault());
         this.calendar = Calendar.getInstance();
         this.setHint("Select Time:");
-        this.timeDialog = new TimePickerDialog(this.getContext(), new TimePickerDialog.OnTimeSetListener() {
+        this.dateDialog = new DatePickerDialog(this.getContext(), new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfDay) {
-                hours = hourOfDay;
-                minutes = minuteOfDay;
-                calendar.set(Calendar.HOUR, hours);
-                calendar.set(Calendar.MINUTE, minutes);
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                setYears(year);
+                setMonths(month);
+                setDays(day);
+                calendar.set(Calendar.YEAR, years);
+                calendar.set(Calendar.MONTH, months);
+                calendar.set(Calendar.DAY_OF_MONTH, days);
                 textView.setText(dateFormat.format(calendar.getTime()));
-                timeDialog.dismiss();
+                dateDialog.dismiss();
             }
-        }, this.hours, this.minutes, this.fullHours);
+        }, years, months, days);
         this.textView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                timeDialog.updateTime(hours, minutes);
-                timeDialog.show();
+                dateDialog.updateDate(years, months, days);
+                dateDialog.show();
             }
         });
     }
@@ -63,16 +65,15 @@ public class MyDateTextView extends MyTextView {
         this.dateFormat.applyPattern(format);
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
+    public void setYears(int years) {
+        this.years = years;
     }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
+    public void setMonths(int months) {
+        this.months = months;
     }
 
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
+    public void setDays(int days) {
+        this.days = days;
     }
-
 }
